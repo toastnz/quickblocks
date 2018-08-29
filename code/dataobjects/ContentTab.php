b27f6c94b8f64fc59379cc98a70f1d469204549c
Danae Miller-Clendon <danae.millerc@gmail.com>
2018-08-30 11:27:43 +1200

    Content tab type

---
+++ code/dataobjects/ContentTab.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Toast\QuickBlocks;
+
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\File;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\TextField;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\FieldType\DBField;
+use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\Security\Permission;
+
+/**
+ * Class ContentTab
+ * @package Toast\QuickBlocks
+ *
+ * @method TabbedContentBlock Parent()
+ * @method File Icon()
+ */
+class ContentTab extends DataObject
+{
+    private static $singular_name = 'Tab';
+    private static $plural_name = 'Tabs';
+    private static $default_sort = 'SortOrder';
+    private static $table_name = 'ContentTab';
+
+    private static $db = [
+        'Title'       => 'Varchar(255)',
+        'Content'     => 'HTMLText',
+        'Description' => 'Text',
+        'SortOrder'   => 'Int'
+    ];
+
+    private static $has_one = [
+        'Parent' => TabbedContentBlock::class
+    ];
+
+    private static $summary_fields = [
+        'Title'           => 'Heading',
+        'Description'     => 'Description',
+        'Content.Summary' => 'Content'
+    ];
+
+    /**
+     * @return FieldList
+     */
+    public function getCMSFields()
+    {
+        $fields = FieldList::create([
+            TextField::create('Title', 'Title'),
+            TextareaField::create('Description', 'Description')
+                ->setRows(2),
+            HTMLEditorField::create('Content', 'Content')
+                ->setRows(15)
+        ]);
+
+        return $fields;
+    }
+
+    public function onBeforeWrite()
+    {
+        // Set max sortorder
+        if (!$this->getField('SortOrder')) {
+            $max = ContentTab::get()->filter(['ParentID' => $this->getField('ParentID')])->max('SortOrder');
+
+            $this->setField('SortOrder', (int)$max + 1);
+        }
+
+        parent::onBeforeWrite();
+    }
+
+    /* ==========================================
+     * CRUD
+     * ========================================*/
+
+    public function canView($member = null)
+    {
+        if ($this->Parent()) {
+            return $this->Parent()->canView($member);
+        }
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canEdit($member = null)
+    {
+        if ($this->Parent()) {
+            return $this->Parent()->canEdit($member);
+        }
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canDelete($member = null)
+    {
+        if ($this->Parent()) {
+            return $this->Parent()->canDelete($member);
+        }
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canCreate($member = null, $context = [])
+    {
+        if ($this->Parent()) {
+            return $this->Parent()->canCreate($member, $context);
+        }
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+}
---
+++ code/dataobjects/TabbedContentBlock.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Toast\QuickBlocks;
+
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldAddExistingAutocompleter;
+use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
+use SilverStripe\Forms\LiteralField;
+use SilverStripe\ORM\FieldType\DBField;
+use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\ORM\HasManyList;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
+
+/**
+ * Class TabbedContentBlock
+ * @package Toast\QuickBlocks
+ *
+ * @method HasManyList Tabs()
+ */
+class TabbedContentBlock extends QuickBlock
+{
+    private static $singular_name = 'Tabbed Content';
+    private static $plural_name = 'Tabbed Content';
+    private static $table_name = 'TabbedContentBlock';
+
+    private static $icon = 'quickblocks/images/utilityblock.png';
+
+    private static $db = [
+        'Colour' => 'Enum("grey,white","white")'
+    ];
+
+    private static $has_many = [
+        'Tabs' => ContentTab::class
+    ];
+
+    public function getCMSFields()
+    {
+        $this->beforeUpdateCMSFields(function ($fields) {
+            if ($this->exists()) {
+                $config = GridFieldConfig_RelationEditor::create(50);
+
+                $config->addComponent(GridFieldOrderableRows::create('SortOrder'))
+                    ->removeComponentsByType(GridFieldDeleteAction::class)
+                    ->addComponent(new GridFieldDeleteAction())
+                    ->removeComponentsByType(GridFieldAddExistingAutoCompleter::class);
+
+                $gridField = GridField::create(
+                    'Tabs',
+                    'Tabs',
+                    $this->Tabs(),
+                    $config
+                );
+
+                $fields->addFieldsToTab('Root.Main', [
+                    DropdownField::create('Colour', 'Background Colour', [
+                        'white' => 'White',
+                        'grey'  => 'Grey'
+                    ]),
+                    $gridField
+                ]);
+            } else {
+                $fields->addFieldToTab('Root.Main',
+                    LiteralField::create('Notice', '<div class="message notice">Save this block to see more options.</div>')
+                );
+            }
+        });
+
+        $fields = parent::getCMSFields();
+        return $fields;
+    }
+
+    public function getContentSummary()
+    {
+        $content = $this->Tabs()->column('Content');
+
+        return DBField::create_field(DBHTMLText::class, implode('<br><br>', $content));
+    }
+}
