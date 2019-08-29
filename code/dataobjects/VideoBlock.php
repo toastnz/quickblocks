c7faa463b9950ed9fb9263027659b4d69b79d8fb
Colin Belfit <colin@toast.co.nz>
2019-08-29 16:15:22 +1200

    Removed the youtubefield requirement. Modified the VideoBlock.

--- code/dataobjects/VideoBlock.php
+++ code/dataobjects/VideoBlock.php
@@ -2,30 +2,34 @@
 
 namespace Toast\QuickBlocks;
 
-use EdgarIndustries\YouTubeField\YouTubeField;
 use SilverStripe\Assets\Image;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
-use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\RequiredFields;
 use SilverStripe\ORM\FieldType\DBField;
-use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\Forms\LiteralField;
+
 
 /**
  * Class VideoBlock
  *
  * @property string Caption
- * @property string VideoID
+ * @property string Video
+ * @property string thumbQualityCms
+ * @property string thumbQualityFront
  */
-class VideoBlock extends QuickBlock
+class VideoBlockExtension extends QuickBlock
 {
     private static $singular_name = 'Video';
-    private static $plural_name = 'Videos';
-    private static $table_name = 'VideoBlock';
+    private static $plural_name   = 'Videos';
+    private static $table_name    = 'VideoBlock';
 
     private static $db = [
         'Caption'   => 'Varchar(255)',
-        'VideoID'   => 'Varchar(50)'
+        'Video'             => 'Text',
+        'thumbQualityCms'   => 'Varchar(20)',
+        'thumbQualityFront' => 'Varchar(20)'
     ];
 
     private static $has_one = [
@@ -43,11 +47,34 @@ class VideoBlock extends QuickBlock
     {
         $this->beforeUpdateCMSFields(function ($fields) {
             $fields->addFieldsToTab('Root.Main', [
-                TextField::create('Caption', 'Caption'),
-                YouTubeField::create('VideoID', 'YouTube Video'),
-                UploadField::create('Thumbnail', 'Thumbnail')
-                    ->setFolderName('Uploads/videos')
-                    ->setDescription('Will automatically use YouTube thumbnail if this image is not uploaded. Ideal size: 960x540')
+                TextField::create('Video', 'Video URL')
+                         ->setDescription('NOTE: Only accepts either YouTube or Vimeo. <br>Paste the whole URL link for the video.'),
+                TextField::create('Caption', 'Caption')
+            ]);
+
+            if ($this->IsVimeo()) {
+                $cmsQualityList = $this->VimeoThumbQuality();
+                $frontendQualityList = $this->VimeoThumbQuality();
+                $title = 'VimeoThumb';
+            } else {
+                $cmsQualityList = $this->YouTubeThumbQuality();
+                $frontendQualityList = $this->YouTubeThumbQuality();
+                $title = 'YouTubeThumb';
+            }
+
+            $fields->addFieldsToTab('Root.Main', [
+                DropdownField::create('thumbQualityCms', 'Thumbnail Size CMS', $cmsQualityList),
+                DropdownField::create('thumbQualityFront', 'Thumbnail Size Front-End', $frontendQualityList)
+                //            UploadField::create('Thumbnail', 'Thumbnail', SS_List($this->VideoThumbnail()))
+            ]);
+
+            $html = '<div class="form-group field text"><label class="form__field-label">&nbsp;</label><div class="form__field-holder"><img src="' . $this->VideoThumbnail($this->thumbQualityCms) . '"></div></div>';
+            $fields->addFieldToTab('Root.Main', LiteralField::create($title, $html));
+
+            $fields->addFieldsToTab('Root.Main', [
+                DropdownField::create('BackgroundColor', 'Background Color', $this->dbObject('BackgroundColor')->enumValues()),
+                DropdownField::create('BackgroundShape', 'Background Shape', $this->dbObject('BackgroundShape')->enumValues()),
+                DropdownField::create('BackgroundAlignment', 'Background Alignment', $this->dbObject('BackgroundAlignment')->enumValues())
             ]);
         });
 
@@ -58,7 +85,7 @@ class VideoBlock extends QuickBlock
 
     public function getCMSValidator()
     {
-        $required = new RequiredFields(['Title', 'VideoID']);
+        $required = new RequiredFields(['Title', 'Video']);
 
         $this->extend('updateCMSValidator',$required);
 
@@ -67,6 +94,165 @@ class VideoBlock extends QuickBlock
 
     public function getContentSummary()
     {
-        return DBField::create_field(DBHTMLText::class, $this->VideoID);
+        return DBField::create_field(DBHTMLText::class, $this->Video);
+    }
+
+    public function checkVideoSource()
+    {
+        $sourceList = [
+            'youtube',
+            'youtu.be',
+            'vimeo'
+        ];
+
+        foreach ($sourceList as $url) {
+            if (stripos($this->Video, $url) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    /**
+     * Check for YouTube clip
+     *
+     * @return bool
+     */
+    public function IsYouTube()
+    {
+        $sourceList = [
+            'youtube',
+            'youtu.be'
+        ];
+        foreach ($sourceList as $url) {
+            if (stripos($this->Video, $url) !== false) {
+
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    /**
+     * Check for Vimeo clip
+     *
+     * @return bool
+     */
+    public function IsVimeo()
+    {
+        if (stripos($this->Video, 'vimeo') !== false) {
+
+            return true;
+        }
+
+        return false;
+
+    }
+
+    /**
+     * Get list of YouTube thumbnail quality levels
+     *
+     * @return array
+     */
+    public function YouTubeThumbQuality()
+    {
+        // Reference: https://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
+
+        $list = [
+            'mqdefault'     => 'Small',
+            'hqdefault'     => 'Medium',
+            'sddefault'     => 'Large',
+            'maxresdefault' => 'HD'
+
+        ];
+
+        return $list;
+    }
+
+    /**
+     * Get list of Vimeo thumbnail quality levels
+     *
+     * @return array
+     */
+    public function VimeoThumbQuality()
+    {
+        $list = [
+            'medium' => 'Small',
+            'large'  => 'Large'
+        ];
+
+        return $list;
+    }
+
+    /**
+     * Get YouTube thumbnail
+     *
+     * @param string $quality
+     * @return string
+     */
+    public function VideoThumbnail($quality = '1')
+    {
+        if ($this->IsVimeo()) {
+            $imageUrl = $this->VimeoThumbnail($quality);
+        } else {
+            $imageUrl = $this->YouTubeThumbnail($quality);
+        }
+
+        return $imageUrl;
+    }
+
+
+    public function YouTubeThumbnail($quality)
+    {
+        $id = str_replace(['https://youtu.be/', 'https://www.youtube.com/watch?v='], '', $this->Video);
+        $imageUrl = 'https://img.youtube.com/vi/' . $id . '/' . $quality . '.jpg';
+
+        return $imageUrl;
+    }
+
+    /**
+     * Get Vimeo thumbnail
+     *
+     * @param $quality
+     * @return string
+     */
+    public function VimeoThumbnail($quality)
+    {
+        $id = trim(str_replace(['https://vimeo.com/', 'https://player.vimeo.com/video/'], '', $this->Video));
+
+        if ($id == '') {
+            return false;
+        }
+
+        $imageUrl = false;
+
+        $apiData = unserialize(file_get_contents("https://vimeo.com/api/v2/video/" . $id . ".php"));
+
+        if (is_array($apiData) && count($apiData) > 0) {
+
+            $videoInfo = $apiData[0];
+
+            switch ($quality) {
+                case 'small':
+                    $imageUrl = $videoInfo['thumbnail_small'];
+                    break;
+                case 'medium':
+                    $imageUrl = $videoInfo['thumbnail_medium'];
+                    break;
+                case 'large':
+                    $imageUrl = $videoInfo['thumbnail_large'];
+                    break;
+                default:
+                    $imageUrl = false;
+                    break;
+            }
+        }
+
+        return $imageUrl;
+
     }
 }
