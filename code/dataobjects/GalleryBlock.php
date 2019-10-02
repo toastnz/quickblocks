df908a12e27771f791dfd0ca4aa65bd6c5a60f7a
Colin Belfit <colin@toast.co.nz>
2019-10-02 14:10:02 +1300

    Added GalleryBlock.

---
+++ code/dataobjects/GalleryBlock.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Toast\QuickBlocks;
+
+use SilverStripe\Assets\Image;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldAddExistingAutocompleter;
+use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
+use SilverStripe\Forms\LiteralField;
+use SilverStripe\ORM\FieldType\DBField;
+use SilverStripe\Forms\RequiredFields;
+use SilverStripe\ORM\FieldType\DBHTMLText;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
+
+/**
+ * Class GalleryBlock
+ *
+ * @method GalleryImageItem Object
+ */
+class GalleryBlock extends QuickBlock
+{
+    private static $singular_name = 'Gallery';
+    private static $plural_name   = 'Galleries';
+    private static $table_name    = 'GalleryBlock';
+
+    private static $has_many = [
+        'GalleryImages' => GalleryImageItem::class
+    ];
+
+    /**
+     * @return FieldList
+     */
+    public function getCMSFields()
+    {
+        $this->beforeUpdateCMSFields(function ($fields) {
+            $config = GridFieldConfig_RelationEditor::create(50)
+                                                    ->removeComponentsByType(GridFieldAddExistingAutoCompleter::class)
+                                                    ->removeComponentsByType(GridFieldDeleteAction::class)
+                                                    ->addComponents(new GridFieldDeleteAction())
+                                                    ->addComponents(GridFieldOrderableRows::create('SortOrder'));
+
+            $grid = GridField::create(
+                'GalleryImages',
+                'Gallery Images',
+                $this->GalleryImages(),
+                $config
+            );
+
+            if ($this->ID) {
+                $fields->addFieldToTab('Root.Main', $grid);
+            } else {
+                $fields->addFieldToTab('Root.Main', LiteralField::create('', '<div class="message notice">Save this block to show additional options.</div>'));
+            }
+        });
+
+        $fields = parent::getCMSFields();
+
+        return $fields;
+    }
+
+
+}
---
+++ code/dataobjects/GalleryImageItem.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Toast\QuickBlocks;
+
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\Forms\TabSet;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\RequiredFields;
+use SilverStripe\Security\Permission;
+use SilverStripe\ORM\DataObject;
+
+/**
+ * Class GalleryImageItem
+ *
+ * @property string Title
+ * @property int SortOrder
+ *
+ * @method GalleryBlock GalleryBlock()
+ */
+class GalleryImageItem extends DataObject
+{
+    private static $singular_name = 'Gallery Image Item';
+    private static $plural_name   = 'Gallery Image Items';
+    private static $default_sort  = 'SortOrder';
+    private static $table_name    = 'GalleryImageItem';
+
+    private static $db = [
+        'Title'     => 'Text',
+        'SortOrder' => 'Int',
+    ];
+
+    private static $has_one = [
+        'GalleryBlock' => GalleryBlock::class,
+        'GalleryImage' => Image::class
+    ];
+
+    private static $owns = [
+        'GalleryImage'
+    ];
+
+    private static $summary_fields = [
+        'Title'                     => 'Title',
+        'GalleryImage.CMSThumbnail' => 'Image'
+    ];
+
+    /**
+     * @return FieldList
+     */
+    public function getCMSFields()
+    {
+        $fields = FieldList::create([new TabSet('Root')]);
+
+        $fields->addFieldsToTab('Root.Main', [
+            TextField::create('Title', 'Title'),
+            UploadField::create('GalleryImage', 'Image')->setFolderName('Uploads/blocks/gallery-images')
+        ]);
+
+        return $fields;
+    }
+
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+    }
+
+    public function getCMSValidator()
+    {
+        return new RequiredFields([Image::class]);
+    }
+
+    /* ==========================================
+     * CRUD
+     * ========================================*/
+
+    public function canView($member = null)
+    {
+        if ($this->Parent()) {
+            return $this->Parent()->canView($member);
+        }
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canEdit($member = null)
+    {
+        if ($this->Parent()) {
+            return $this->Parent()->canEdit($member);
+        }
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canDelete($member = null)
+    {
+        if ($this->Parent()) {
+            return $this->Parent()->canDelete($member);
+        }
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canCreate($member = null, $context = [])
+    {
+        if ($this->Parent()) {
+            return $this->Parent()->canCreate($member, $context);
+        }
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+}
