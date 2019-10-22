7957b4a4dffc158f2eb095d9c2b4d84425f6edd7
Delma <delma@toast.co.nz>
2019-10-22 15:33:49 +1300

    News Block

--- code/dataobjects/NewsBlock.php
+++ code/dataobjects/NewsBlock.php
@@ -15,39 +15,34 @@ use SilverStripe\Forms\GridField\GridFieldDeleteAction;
 use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
 use SilverStripe\Forms\TextField;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
-use Toast\Model\LinkBlockItem;
+use Toast\Model\NewsBlockItem;
 
 
-class LinkBlock extends QuickBlock
+class NewsBlock extends QuickBlock
 {
-    private static $singular_name = 'Link';
-    private static $plural_name = 'Links';
-    private static $table_name = 'LinkBlock';
+    private static $singular_name = 'News';
+    private static $plural_name = 'News';
+    private static $table_name = 'NewsBlock';
     private static $icon = 'quickblocks/images/image.png';
 
-    private static $db = [
-        'Title'     => 'Varchar(100)',
-        'Content'   => 'HTMLText',
-    ];
 
     private static $has_many = [
-        'Items' => LinkBlockItem::class
+        'Items' => NewsBlockItem::class
     ];
-
 
     public function getCMSFields()
     {
-        $LinkConfig = GridFieldConfig_RelationEditor::create(10);
-        $LinkConfig->addComponent(GridFieldOrderableRows::create('SortOrder'))
+        $NewsConfig = GridFieldConfig_RelationEditor::create(10);
+        $NewsConfig->addComponent(GridFieldOrderableRows::create('SortOrder'))
             ->removeComponentsByType(GridFieldDeleteAction::class)
             ->addComponent(new GridFieldDeleteAction(false))
             ->removeComponentsByType('GridFieldAddExistingAutoCompleter');
 
-        $LinkBlockGridField = GridField::create(
+        $NewsBlockGridField = GridField::create(
             'Items',
-            'Link Block Items',
+            'News Block Items',
             $this->owner->Items(),
-            $LinkConfig
+            $NewsConfig
         );
 
         /**
@@ -55,18 +50,13 @@ class LinkBlock extends QuickBlock
          */
         $fields = parent::getCMSFields();
 
-        $fields->addFieldsToTab('Root.Main', [
-            TextField::create('Title', 'Title'),
-            HTMLEditorField::create('Content', 'Content')
-                ->setRows(15),
-        ]);
 
         /** -----------------------------------------
          * Slide Details
          * ----------------------------------------*/
         if ($this->ID !== 0) {
             $fields->addFieldsToTab('Root.Main', [
-                $LinkBlockGridField
+                $NewsBlockGridField
             ]);
         }
 
---
+++ code/dataobjects/NewsBlockItem.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: staff
+ * Date: 1/02/19
+ * Time: 3:43 PM
+ */
+
+namespace Toast\Model;
+
+
+use Sheadawson\Linkable\Forms\LinkField;
+use Sheadawson\Linkable\Models\Link;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\ORM\DataObject;
+use Toast\QuickBlocks\NewsBlock;
+
+
+class NewsBlockItem extends  DataObject
+{
+    private static $table_name = 'NewsBlockItem';
+    private static $db = [
+        'SortOrder' => 'Int',
+        'Title'     => 'Varchar(100)',
+        'Content'   => 'HTMLText',
+    ];
+
+    private static $has_one = [
+        'Link'      => Link::class,
+        'Image'     => Image::class,
+        'NewsBlock' => NewsBlock::class
+    ];
+
+    private static $summary_fields = [
+        'Image.CMSThumbnail' => 'Thumbnail',
+        'Title'              => 'Title',
+    ];
+    private static $owns = [
+        'Image'
+    ];
+    /**
+     * @return FieldList
+     */
+    public function getCMSFields()
+    {
+
+        /**
+         * @var FieldList $fields
+         */
+        $fields = parent::getCMSFields();
+
+        $fields->removeByName([
+            'SortOrder',
+            'ParentPageID',
+            'NewsBlockID',
+            'FileTracking',
+            'LinkTracking',
+        ]);
+
+        /** =========================================
+         * @var FieldList $fields
+         * ========================================*/
+
+        $fields->addFieldsToTab('Root.Main', [
+            TextField::create('Title', 'Title'),
+            LinkField::create('LinkID','Link'),
+            HTMLEditorField::create('Content', 'Content')->setRows(5),
+            UploadField::create('Image', 'Image')
+                ->setDescription('Ideal size at least 510px * 510px')
+                ->setFolderName('Uploads/Images')
+        ]);
+
+
+        return $fields;
+    }
+
+}
