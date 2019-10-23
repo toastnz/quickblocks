6cbb99438d769624536dc3e7b421e52a8bc35572
Delma <delma@toast.co.nz>
2019-10-23 13:27:17 +1300

    tweaks

--- code/dataobjects/ColumnBlock.php
+++ code/dataobjects/ColumnBlock.php
@@ -24,26 +24,13 @@ class ColumnBlock extends QuickBlock
         'Content' => 'HTMLText',
         'Columns' => 'Enum("1,2,3", "3")'
     ];
-    private static $has_many = [
-        'ColumnItems' => ColumnBlockItem::class
-    ];
+
     /**
      * @return FieldList
      */
 
     public function getCMSFields()
     {
-//        $config = GridFieldConfig_RelationEditor::create(50)
-//            ->removeComponentsByType(GridFieldAddExistingAutoCompleter::class)
-//            ->removeComponentsByType(GridFieldDeleteAction::class)
-//            ->addComponents(new GridFieldDeleteAction())
-//            ->addComponents(GridFieldOrderableRows::create('SortOrder'));
-//        $grid = GridField::create(
-//            'ColumnItems',
-//            'Column Items',
-//            $this->ColumnItems(),
-//            $config
-//        );
 
         $fields = parent::getCMSFields();
         $fields->addFieldsToTab('Root.Main', [
@@ -51,7 +38,6 @@ class ColumnBlock extends QuickBlock
             HTMLEditorField::create('Content', 'Content')
                 ->setRows(15),
             DropdownField::create('Columns', 'Columns', singleton(ColumnBlock::class)->dbObject('Columns')->enumValues()),
-           // $grid
         ]);
         return $fields;
     }
