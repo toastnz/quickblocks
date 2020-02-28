9b45f0060fee8f684769a0b66e439e5e94ece21b
Colin Belfit <colin@toast.co.nz>
2020-02-28 15:57:06 +1300

    Updated the BlockTablesMigrationTask tables array.

--- src/Tasks/BlockTablesMigrationTask.php
+++ src/Tasks/BlockTablesMigrationTask.php
@@ -18,16 +18,34 @@ class BlockTablesMigrationTask extends BuildTask
     {
         $oldBlockTables = [
             'AccordionBlock',
+            'AccordionBlock_Live',
+            'AccordionBlock_Versions',
             'AccordionItem',
             'ContentTab',
             'DownloadBlock',
+            'DownloadBlock_Live',
+            'DownloadBlock_Versions',
             'ImageBlock',
+            'ImageBlock_Live',
+            'ImageBlock_Versions',
             'QuickBlock',
+            'QuickBlock_Live',
+            'QuickBlock_Versions',
             'SplitBlock',
+            'SplitBlock_Live',
+            'SplitBlock_Versions',
             'TabbedContentBlock',
+            'TabbedContentBlock_Live',
+            'TabbedContentBlock_Versions',
             'TestimonialBlock',
+            'TestimonialBlock_Live',
+            'TestimonialBlock_Versions',
             'TextBlock',
-            'VideoBlock'
+            'TextBlock_Live',
+            'TextBlock_Versions',
+            'VideoBlock',
+            'VideoBlock_Live',
+            'VideoBlock_Versions'
         ];
 
         foreach ($oldBlockTables as $table) {
