3462ea7893d4084b4031239f8a2d36f89cb6f923
Colin Belfit <colin@toast.co.nz>
2020-03-17 12:53:21 +1300

    Moved the TestimonialBlockExtension code into the TestimonialBlock.

--- code/dataobjects/TestimonialBlock.php
+++ code/dataobjects/TestimonialBlock.php
@@ -8,6 +8,11 @@ use SilverStripe\Forms\TextareaField;
 use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\FieldType\DBField;
 use SilverStripe\Forms\RequiredFields;
+use Toast\Model\Testimonial;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
+use UndefinedOffset\SortableGridField\Forms\GridFieldSortableRows;
+
 
 /**
  * Class TestimonialBlock
@@ -28,21 +33,36 @@ class TestimonialBlock extends QuickBlock
         'ShowSlider'      => 'Boolean'
     ];
 
+    private static $many_many = [
+        'Testimonials' => Testimonial::class,
+    ];
+
     /**
      * @return FieldList
      */
     public function getCMSFields()
     {
         $this->beforeUpdateCMSFields(function ($fields) {
+
             $fields->addFieldsToTab('Root.Main', [
                 TextareaField::create('Testimonial', 'Testimonial'),
                 TextField::create('Attribution', 'Name'),
                 TextField::create('Location', 'Location'),
                 CheckboxField::create('ShowSlider', 'Show Slider'),
             ]);
+
+            $conf = GridFieldConfig_RelationEditor::create(10);
+            $conf->addComponent(new GridFieldSortableRows('SortOrder'));
+
+            $fields->addFieldsToTab('Root.Testimonials', [
+                GridField::create('Testimonials', 'Testimonials', $this->owner->Testimonials(), $conf)
+            ]);
+
         });
 
         $fields = parent::getCMSFields();
+
+        $fields->removeByName(['Attribution', 'Testimonial', 'Location']);
 
         return $fields;
     }
